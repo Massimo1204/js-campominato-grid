3b71ea2f9354f7a9f029ea288c90fb2585228317
Massimo1204 <xiangmassimo@gmail.com>
2022-03-07 16:31:59 +0100

    campo minato

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const mode = document.getElementById('mode');
+document.getElementById('play-button').addEventListener('click', startGame);
+
+function startGame(){
+    createField(chooseMode(mode.value));
+}
+
+function chooseMode(mode){
+    let fieldSize;
+    switch(mode){
+        case 'facile' :
+            fieldSize = 100;
+            break;
+        case 'normale' :
+            fieldSize = 81;
+            break;
+        case 'difficile' :
+            fieldSize = 49;
+            break;    
+    }
+    return fieldSize;
+}
+
+function clicked(){
+    this.classList.add('safe');
+}
+
+function createField(size){
+    for(let i=0;i<size;i++){
+        const newBox = document.createElement('div');
+        newBox.classList.add('box');
+        newBox.style.height = "calc(100% / "+ Math.sqrt(size)+")";
+        newBox.style.width = "calc(100% / "+ Math.sqrt(size)+")";
+        newBox.innerHTML= i+1;
+        document.getElementById('game-field').appendChild(newBox);
+        newBox.addEventListener('click', clicked);
+    }
+}
